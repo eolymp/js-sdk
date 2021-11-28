72cc621c1c02dce457d4eb96fab09fcb3238784f
Sergey Kolodyazhnyy <sergey.kolodyazhnyy@gmail.com>
2021-11-28 19:22:27 +0100

    Update from contracts

--- services/typewriter/block.d.ts
+++ services/typewriter/block.d.ts
@@ -1,22 +1,22 @@
-import { Leaf } from "./leaf";
-export declare type Block = {
-    paragraph?: Paragraph;
-    layout?: Layout;
-    code?: Code;
-    math?: Math;
-    image?: Image;
-    heading?: Heading;
-    list?: List;
-    embed?: Embed;
-    comment?: Comment;
-};
+import { Inline } from "./inline";
 export declare type Container = {
     children?: Block[];
 };
-export declare type Paragraph = {
-    children?: Leaf[];
+export declare type Block = {
+    paragraph?: Block_Paragraph;
+    layout?: Block_Layout;
+    code?: Block_Code;
+    math?: Block_Math;
+    image?: Block_Image;
+    heading?: Block_Heading;
+    list?: Block_List;
+    embed?: Block_Embed;
+    comment?: Block_Comment;
 };
-export declare type Layout = {
+export declare type Block_Paragraph = {
+    children?: Inline[];
+};
+export declare type Block_Layout = {
     direction?: string;
     alignItems?: string;
     alignContent?: string;
@@ -26,36 +26,37 @@ export declare type Layout = {
     padding?: number[];
     children?: Container[];
 };
-export declare type List = {
+export declare type Block_List = {
     style?: string;
     children?: Container[];
 };
-export declare type Image = {
+export declare type Block_Image = {
     src?: string;
     alt?: string;
     title?: string;
     width?: number;
     height?: number;
+    zoomable?: boolean;
 };
-export declare type Heading = {
+export declare type Block_Heading = {
     level?: number;
-    children?: Leaf[];
+    children?: Inline[];
 };
-export declare type Math = {
+export declare type Block_Math = {
     content?: string;
 };
-export declare type Code = {
+export declare type Block_Code = {
     lang?: string;
     content?: string;
 };
-export declare type Comment = {
+export declare type Block_Comment = {
     content?: string;
 };
-export declare type Embed = {
+export declare type Block_Embed = {
     path?: string;
-    values?: Embed_Value[];
+    values?: Block_Embed_Value[];
 };
-export declare type Embed_Value = {
+export declare type Block_Embed_Value = {
     name?: string;
     value?: string;
 };
---
+++ services/typewriter/inline.d.ts
@@ -0,0 +1,33 @@
+export declare type Inline = {
+    text?: Inline_Text;
+    link?: Inline_Link;
+    math?: Inline_Math;
+    code?: Inline_Code;
+    variable?: Inline_Variable;
+};
+export declare type Inline_Style = {
+    bold?: boolean;
+    italic?: boolean;
+    underline?: boolean;
+    strikethrough?: boolean;
+    fixed?: boolean;
+    math?: boolean;
+};
+export declare type Inline_Text = {
+    str?: string;
+    style?: Inline_Style;
+};
+export declare type Inline_Link = {
+    url?: string;
+    children?: Inline[];
+};
+export declare type Inline_Math = {
+    content?: string;
+};
+export declare type Inline_Code = {
+    content?: string;
+};
+export declare type Inline_Variable = {
+    name?: string;
+    style?: Inline_Style;
+};
