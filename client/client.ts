35dbe115be9e5279c2c8bc5ed5964217715b74eb
Sergey Kolodyazhnyy <sergey.kolodyazhnyy@gmail.com>
2022-07-06 11:10:10 +0200

    Add retries and authentication to HTTP client

--- client/client.ts
+++ client/client.ts
@@ -1,97 +1,128 @@
 import {BadRequestError, ForbiddenError, InternalServerError, NotFoundError, UnauthorizedError} from "./errors";
 
+const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
+
 export class Options {
-    headers?: Record<string, string>;
+  retry?: number;
+  headers?: Record<string, string>;
+  token?: string;
+  authenticate?: () => Promise<string>;
 }
 
 export class Client {
-    private readonly url: string;
-    private readonly headers: Record<string, string>;
+  private readonly url: string;
+  private readonly retry: number;
+  private readonly headers: Record<string, string>;
+  private readonly authenticate?: () => Promise<string>;
 
-    constructor(url: string, opts: Options = {}) {
-        if (!url.endsWith('/')) {
-            url += '/';
+  private token?: string;
+
+  constructor(url: string, opts: Options = {}) {
+    if (!url.endsWith('/')) {
+      url += '/';
+    }
+
+    this.url = url;
+    this.retry = opts.retry || 3;
+    this.token = opts.token || undefined;
+    this.headers = opts.headers || {};
+    this.authenticate = opts.authenticate || undefined;
+  }
+
+  async do<T>(method: string, path: string, body: string, headers: Record<string, string> = {}): Promise<T> {
+    const url = this.url + (path.startsWith('/') ? path.substr(1) : path);
+
+    if (!this.token && this.authenticate) {
+      this.token = await this.authenticate()
+    }
+
+    const max = Math.max(1, this.retry);
+    for (let retry = 0; retry < max; retry++) {
+      const last = retry >= max-1
+
+      const auth: Record<string, string> = {}
+      if (this.token) {
+        auth['Authorization'] = `Bearer ${this.token}`;
+      }
+
+      const params: RequestInit = {
+        method: method,
+        mode: 'cors',
+        credentials: 'omit',
+        headers: {
+          'Content-Type': 'application/json',
+          ...auth,
+          ...this.headers,
+          ...(headers || {}),
+        },
+        redirect: 'follow',
+        body: body,
+      };
+
+      const response = await fetch(url, params);
+      if (response.status === 404) {
+        const {message} = await Client.getResponseErrorData(response, 'not found')
+        throw new NotFoundError(message);
+      }
+
+      if (response.status === 401) {
+        if (retry === 0 && this.authenticate) { // if we got 401 on first attempt, reset token and retry
+          this.token = undefined;
+          continue;
         }
 
-        this.url = url;
-        this.headers = opts.headers || {};
+        const {message} = await Client.getResponseErrorData(response, 'unauthorized')
+        throw new UnauthorizedError(message)
+      }
+
+      if (response.status === 403) {
+        const {message} = await Client.getResponseErrorData(response, 'forbidden')
+        throw new ForbiddenError(message)
+      }
+
+      if (response.status === 429 && !last) {
+        await sleep((retry + 1) * 1000)
+        continue
+      }
+
+      if (response.status >= 400 && response.status < 500) {
+        const {
+          message,
+          code,
+          status,
+          details
+        } = await Client.getResponseErrorData(response, `bad request (${response.status})`)
+        throw new BadRequestError(message, code, status, details);
+      }
+
+      if (response.status >= 500) {
+        const {
+          message,
+          code,
+          details
+        } = await Client.getResponseErrorData(response, `server error (${response.status})`)
+        throw new InternalServerError(message, code, response.status, details);
+      }
+
+      return await response.json();
     }
 
-    do<T>(method: string, path: string, body: string, headers: Record<string, string> = {}): Promise<T> {
-        const url = this.url + (path.startsWith('/') ? path.substr(1) : path);
+    return {} as T;
+  }
 
-        const params: RequestInit = {
-            method: method,
-            mode: 'cors',
-            credentials: 'omit',
-            headers: {
-                'Content-Type': 'application/json',
-                ...this.headers,
-                ...(headers || {}),
-            },
-            redirect: 'follow',
-            body: body,
-        };
+  graphql<R>(query: string, variables: Record<string, any>, opts?: Options) {
+    return this.do<R>("POST", "/graphql", JSON.stringify({query, variables}), opts?.headers);
+  }
 
-        return fetch(url, params).then((response) => {
-            if (response.status === 404) {
-                return response
-                  .json()
-                  .then(({ message }) => Promise.reject(new NotFoundError(message)))
-                  .catch(() => Promise.reject(new NotFoundError('not found')));
-            }
+  call<R, E>(method: string, input: R, opts?: Options): Promise<E> {
+    return this.do<E>("POST", "/" + method, JSON.stringify(input), opts?.headers)
+  }
 
-            if (response.status === 401) {
-                return response
-                  .json()
-                  .then(({ message }) => Promise.reject(new UnauthorizedError(message)))
-                  .catch((e) => Promise.reject(e instanceof UnauthorizedError ? e : new UnauthorizedError('unauthorized')));
-            }
-
-            if (response.status === 403) {
-                return response
-                  .json()
-                  .then(({ message }) => Promise.reject(new ForbiddenError(message)))
-                  .catch((e) => Promise.reject(e instanceof ForbiddenError ? e : new ForbiddenError('forbidden')));
-            }
-
-            if (response.status >= 400 && response.status < 500) {
-                return response
-                  .json()
-                  .then(({ message, code, status, details }) => Promise.reject(new BadRequestError(message, code, status, details)))
-                  .catch((e) =>
-                    Promise.reject(
-                      e instanceof BadRequestError
-                        ? e
-                        : new BadRequestError(`server error (${response.status})`, undefined, response.status)
-                    )
-                  );
-            }
-
-            if (response.status >= 500) {
-                return response
-                  .json()
-                  .then(({ message, code, details }) =>
-                    Promise.reject(new InternalServerError(message, code, response.status, details))
-                  )
-                  .catch((e) =>
-                    Promise.reject(
-                      e instanceof InternalServerError
-                        ? e
-                        : new InternalServerError(`server error (${response.status})`, undefined, response.status)
-                    )
-                  );
-            }
-
-            return Promise.resolve(response.json());
-        });
+  private static async getResponseErrorData(response: Response, message: string): Promise<Record<string, any>> {
+    try {
+      return await response.json()
+    } catch (error) {
+      return {message, error}
     }
-
-    graphql<R>(query: string, variables: Record<string, any>, opts?: Options) {
-        return this.do<R>("POST", "/graphql", JSON.stringify({query, variables}), opts?.headers);
-    }
-
-    call<R, E>(method: string, input: R, opts?: Options): Promise<E> {
-        return this.do<E>("POST", "/" + method, JSON.stringify(input), opts?.headers)
-    }
+  }
 }
