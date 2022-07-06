deafa09e48160ed71903897b4ebb04d2bbdc7c6e
Sergey Kolodyazhnyy <sergey.kolodyazhnyy@gmail.com>
2022-07-06 11:11:37 +0200

    v0.4.23

--- client/client.d.ts
+++ client/client.d.ts
@@ -1,11 +1,18 @@
 export declare class Options {
+    retry?: number;
     headers?: Record<string, string>;
+    token?: string;
+    authenticate?: () => Promise<string>;
 }
 export declare class Client {
     private readonly url;
+    private readonly retry;
     private readonly headers;
+    private readonly authenticate?;
+    private token?;
     constructor(url: string, opts?: Options);
     do<T>(method: string, path: string, body: string, headers?: Record<string, string>): Promise<T>;
     graphql<R>(query: string, variables: Record<string, any>, opts?: Options): Promise<R>;
     call<R, E>(method: string, input: R, opts?: Options): Promise<E>;
+    private static getResponseErrorData;
 }
--- client/client.js
+++ client/client.js
@@ -1,7 +1,17 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Client = exports.Options = void 0;
 const errors_1 = require("./errors");
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 class Options {
 }
 exports.Options = Options;
@@ -11,54 +21,64 @@ class Client {
             url += '/';
         }
         this.url = url;
+        this.retry = opts.retry || 3;
+        this.token = opts.token || undefined;
         this.headers = opts.headers || {};
+        this.authenticate = opts.authenticate || undefined;
     }
     do(method, path, body, headers = {}) {
-        const url = this.url + (path.startsWith('/') ? path.substr(1) : path);
-        const params = {
-            method: method,
-            mode: 'cors',
-            credentials: 'omit',
-            headers: Object.assign(Object.assign({ 'Content-Type': 'application/json' }, this.headers), (headers || {})),
-            redirect: 'follow',
-            body: body,
-        };
-        return fetch(url, params).then((response) => {
-            if (response.status === 404) {
-                return response
-                    .json()
-                    .then(({ message }) => Promise.reject(new errors_1.NotFoundError(message)))
-                    .catch(() => Promise.reject(new errors_1.NotFoundError('not found')));
+        return __awaiter(this, void 0, void 0, function* () {
+            const url = this.url + (path.startsWith('/') ? path.substr(1) : path);
+            if (!this.token && this.authenticate) {
+                this.token = yield this.authenticate();
             }
-            if (response.status === 401) {
-                return response
-                    .json()
-                    .then(({ message }) => Promise.reject(new errors_1.UnauthorizedError(message)))
-                    .catch((e) => Promise.reject(e instanceof errors_1.UnauthorizedError ? e : new errors_1.UnauthorizedError('unauthorized')));
+            const max = Math.max(1, this.retry);
+            for (let retry = 0; retry < max; retry++) {
+                const last = retry >= max - 1;
+                const auth = {};
+                if (this.token) {
+                    auth['Authorization'] = `Bearer ${this.token}`;
+                }
+                const params = {
+                    method: method,
+                    mode: 'cors',
+                    credentials: 'omit',
+                    headers: Object.assign(Object.assign(Object.assign({ 'Content-Type': 'application/json' }, auth), this.headers), (headers || {})),
+                    redirect: 'follow',
+                    body: body,
+                };
+                const response = yield fetch(url, params);
+                if (response.status === 404) {
+                    const { message } = yield Client.getResponseErrorData(response, 'not found');
+                    throw new errors_1.NotFoundError(message);
+                }
+                if (response.status === 401) {
+                    if (retry === 0 && this.authenticate) { // if we got 401 on first attempt, reset token and retry
+                        this.token = undefined;
+                        continue;
+                    }
+                    const { message } = yield Client.getResponseErrorData(response, 'unauthorized');
+                    throw new errors_1.UnauthorizedError(message);
+                }
+                if (response.status === 403) {
+                    const { message } = yield Client.getResponseErrorData(response, 'forbidden');
+                    throw new errors_1.ForbiddenError(message);
+                }
+                if (response.status === 429 && !last) {
+                    yield sleep((retry + 1) * 1000);
+                    continue;
+                }
+                if (response.status >= 400 && response.status < 500) {
+                    const { message, code, status, details } = yield Client.getResponseErrorData(response, `bad request (${response.status})`);
+                    throw new errors_1.BadRequestError(message, code, status, details);
+                }
+                if (response.status >= 500) {
+                    const { message, code, details } = yield Client.getResponseErrorData(response, `server error (${response.status})`);
+                    throw new errors_1.InternalServerError(message, code, response.status, details);
+                }
+                return yield response.json();
             }
-            if (response.status === 403) {
-                return response
-                    .json()
-                    .then(({ message }) => Promise.reject(new errors_1.ForbiddenError(message)))
-                    .catch((e) => Promise.reject(e instanceof errors_1.ForbiddenError ? e : new errors_1.ForbiddenError('forbidden')));
-            }
-            if (response.status >= 400 && response.status < 500) {
-                return response
-                    .json()
-                    .then(({ message, code, status, details }) => Promise.reject(new errors_1.BadRequestError(message, code, status, details)))
-                    .catch((e) => Promise.reject(e instanceof errors_1.BadRequestError
-                    ? e
-                    : new errors_1.BadRequestError(`server error (${response.status})`, undefined, response.status)));
-            }
-            if (response.status >= 500) {
-                return response
-                    .json()
-                    .then(({ message, code, details }) => Promise.reject(new errors_1.InternalServerError(message, code, response.status, details)))
-                    .catch((e) => Promise.reject(e instanceof errors_1.InternalServerError
-                    ? e
-                    : new errors_1.InternalServerError(`server error (${response.status})`, undefined, response.status)));
-            }
-            return Promise.resolve(response.json());
+            return {};
         });
     }
     graphql(query, variables, opts) {
@@ -66,6 +86,16 @@ class Client {
     }
     call(method, input, opts) {
         return this.do("POST", "/" + method, JSON.stringify(input), opts === null || opts === void 0 ? void 0 : opts.headers);
+    }
+    static getResponseErrorData(response, message) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return yield response.json();
+            }
+            catch (error) {
+                return { message, error };
+            }
+        });
     }
 }
 exports.Client = Client;
